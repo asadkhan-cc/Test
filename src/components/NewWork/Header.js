fe44e6b6b68712d503479cb0cb21570a1ea73ed8
asadkhan-cc <asad.khan@cooperativecomputing.com>
2022-07-20 17:16:45 +0500

    minor import component bug fix

---
+++ src/components/NewWork/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header>
+        this is a nav bar
+        <br />
+        <ul className="header ">
+          <span>image</span>
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>BLOG</li>
+          <span>Search</span>
+        </ul>
+      </header>
+      </>
+  );
+}
+
+export default Header
