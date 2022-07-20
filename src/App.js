41d4ef352bc30e9e289a355670752071be93517d
asadkhan-cc <asad.khan@cooperativecomputing.com>
2022-07-20 16:48:33 +0500

    header made

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
 // import Main from "./components/Main/Main";
-import './App.css'
+import "./App.css";
 function App() {
-
-  
   return (
-    <div >
-      <header>this is a nav bar</header>
-    {/* <Main className="App-header"></Main> */}
+    <div>
+      <header>
+        this is a nav bar
+        <br />
+        <ul className="header ">
+          <span>image</span>
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>BLOG</li>
+          <span>Search</span>
+        </ul>
+      </header>
+      {/* <Main className="App-header"></Main> */}
     </div>
   );
 }
